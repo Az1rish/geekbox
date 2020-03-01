58ab47a03360fba33f5d35b88b6bd4b92f1242df
Shane <shanemcneil19@gmail.com>
2020-03-01 07:09:07 -0700

    add BrowserRouter to index.js file and update App.test file, passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-      <App />, div
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div
   );
   ReactDOM.unmountComponentAtNode(div);
 });
