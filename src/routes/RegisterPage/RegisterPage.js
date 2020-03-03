5f116224575157f453c3a5c1d8245058fd04576f
Shane <shanemcneil19@gmail.com>
2020-03-03 05:43:16 -0700

    create RegisterPage

---
+++ src/routes/RegisterPage/RegisterPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import './RegisterPage.css';
+
+export default class RegisterPage extends Component {
+    static defaultProps = {
+        history: {
+            push: () => {}
+        }
+    }
+
+    handleRegister = () => {
+        const { history } = this.props;
+        history.push('/signin');
+    }
+
+    render() {
+        return (
+            <section className="RegisterPage">
+                <h2>Register</h2>
+                <RegistrationForm
+                    onRegister={this.handleRegister}
+                />
+            </section>
+        );
+    }
+}
