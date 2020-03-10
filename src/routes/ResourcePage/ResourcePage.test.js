f14ae921733b5415b1f7c2b3e3777cd15c007d44
Shane <shanemcneil19@gmail.com>
2020-03-10 07:08:53 -0700

    all tests passing

--- src/routes/ResourcePage/ResourcePage.test.js
+++ src/routes/ResourcePage/ResourcePage.test.js
@@ -2,20 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import ResourceListPage from '../ResourceListPage/ResourceListPage';
+import CategoryListPage from '../CategoryListPage/CategoryListPage';
 import ResourcePage from './ResourcePage';
 
 test.only('renders without crashing', () => {
   const div = document.createElement('div');
-  const location = {
-      state: {
-          category: 1
-      }
-  }
   ReactDOM.render(
       <BrowserRouter>
-        <ResourceListPage>
-            <ResourcePage location={location} />
-        </ResourceListPage>
+        <CategoryListPage>
+            <ResourceListPage>
+                <ResourcePage />
+            </ResourceListPage>
+        </CategoryListPage>
       </BrowserRouter>, 
       div
   );
