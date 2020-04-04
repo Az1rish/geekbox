1eef69be8f4792ba8b4da8c7d7f9e8c941441b4c
Shane <shanemcneil19@gmail.com>
2020-04-04 10:03:38 -0700

    clarify landing page instructions

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TokenService from '../../services/token-service';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 export default function LandingPage() {
     return (
@@ -10,6 +11,18 @@ export default function LandingPage() {
             </section>
             <section>
                 <p>Find a resource for what you'd like to learn</p>
+                <p>We strive to make this page as easy and accessible as possible. Navigation flows like this:</p>
+                <ul className="instructions">
+                    <li className="innerLeft">If you don't want to sign in:
+                        <ul className="innerInstructions">
+                            <li className="innerLeft">Click "Categories" in the nav bar above or "Enter GeekBox" below.</li>
+                            <li className="innerLeft">You will then be taken to a page with buttons that represent the currently available categories. Click a category you're interested in to see the resources that have been added</li>
+                            <li className="innerLeft">You will then be taken to a page similar to the "Categories" page but this time it will be a list of "Resources" that you can click on. Click one that looks interesting to you.</li>
+                            <li className="innerLeft">On the resource page you will see the name of that resource, a link to their website, a description of the resource and any comments/reviews that other users have left.</li>
+                        </ul>
+                    </li>
+                    <li className="innerLeft">If you'd like to register an account and sign in navigation is the same as above but you will also have the capability to add categories, resources and comments/reviews.</li>
+                </ul>
                 <p>You can register and sign in if you'd like to add a category, resource, comment or rating</p>
                 {TokenService.hasAuthToken()
                     ? <p className="black">You are already Signed In.</p>
